e6c15328d403109a42363bae92bf4a6f97935ec4
ArmSal <152378147+ArmSal@users.noreply.github.com>
2025-04-14 00:20:27 +0200

    Ajout du CRUD (Article , Message)

--- src/Controller/Admin/MessageController.php
+++ src/Controller/Admin/MessageController.php
@@ -2,17 +2,80 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\ContactMessage;
+use App\Form\ContactMessageType;
+use App\Repository\ContactMessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
-final class MessageController extends AbstractController{
-    #[Route('/admin/message', name: 'app_admin_message')]
-    public function index(): Response
+#[Route('/admin/message')]
+final class MessageController extends AbstractController
+{
+    #[Route(name: 'app_message_index', methods: ['GET'])]
+    public function index(ContactMessageRepository $contactMessageRepository): Response
     {
         return $this->render('admin/message/index.html.twig', [
-            'controller_name' => 'Admin/MessageController',
+            'contact_messages' => $contactMessageRepository->findAll(),
         ]);
+    }
+
+    #[Route('/new', name: 'app_message_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $contactMessage = new ContactMessage();
+        $form = $this->createForm(ContactMessageType::class, $contactMessage);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($contactMessage);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_message_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/message/new.html.twig', [
+            'contact_message' => $contactMessage,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_message_show', methods: ['GET'])]
+    public function show(ContactMessage $contactMessage): Response
+    {
+        return $this->render('admin/message/show.html.twig', [
+            'contact_message' => $contactMessage,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_message_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, ContactMessage $contactMessage, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ContactMessageType::class, $contactMessage);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_message_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/message/edit.html.twig', [
+            'contact_message' => $contactMessage,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_message_delete', methods: ['POST'])]
+    public function delete(Request $request, ContactMessage $contactMessage, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$contactMessage->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($contactMessage);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_message_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\ContactMessage;
+use App\Form\ContactMessageType;
+use App\Repository\ContactMessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $contactMessage = new ContactMessage();
+        $form = $this->createForm(ContactMessageType::class, $contactMessage);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($contactMessage);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_message_index', [], Response::HTTP_SEE_OTHER);
+        }
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'contact_message' => $contactMessage,
+            'form' => $form,
         ]);
     }
 }
