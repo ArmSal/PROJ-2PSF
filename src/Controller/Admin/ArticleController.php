368f28d1521c6adb37a62cfab4847d1c25a95aad
ArmSal <152378147+ArmSal@users.noreply.github.com>
2025-04-13 13:45:03 +0200

    contrôleurs ajoutés
    Contrôleurs ajoutés
    Home, Blog, Contact, Admin/(Article, Message)

---
+++ src/Controller/Admin/ArticleController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ArticleController extends AbstractController{
+    #[Route('/admin/article', name: 'app_admin_article')]
+    public function index(): Response
+    {
+        return $this->render('admin/article/index.html.twig', [
+            'controller_name' => 'Admin/ArticleController',
+        ]);
+    }
+}
