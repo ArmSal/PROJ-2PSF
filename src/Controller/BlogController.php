ea2c8bfa840cffc01e0d8146f35d6b2abe03a001
ArmSal <152378147+ArmSal@users.noreply.github.com>
2025-04-16 01:07:39 +0200

    Update blog Template,

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class BlogController extends AbstractController
+class BlogController extends AbstractController
 {
     #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findBy([], ['createdAt' => 'DESC']);
+
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'articles' => $articles,
+        ]);
+    }
+
+    #[Route('/blog/article/{id}', name: 'blog_article_show')]
+    public function show(Article $article): Response
+    {
+        return $this->render('blog/show.html.twig', [
+            'article' => $article,
         ]);
     }
 }
